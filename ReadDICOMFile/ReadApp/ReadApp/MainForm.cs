d758bba4c7712fa96bf804e2707edf44d287f91d
QNghia <nghianq.it@gmail.com>
2017-09-28 00:02:29 +0700

    update app c# read dicom file

---
+++ ReadDICOMFile/ReadApp/ReadApp/MainForm.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+using Dicom;
+using Dicom.Imaging;
+
+namespace ReadApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            var file = DicomFile.Open("result.dcm");
+            var name = file.Dataset.Get<string>(DicomTag.PatientAddress);
+            var image = new DicomImage("goodSample");
+            for (int i = 0; i < image.NumberOfFrames; i++)
+            {
+                image.RenderImage(i).AsBitmap().Save("/result/" + i + "result.jpg");
+            }
+            System.Console.WriteLine(name);
+        }
+    }
+}
--- ReadDICOMFile_Matlab/main.m
+++
@@ -1,4 +0,0 @@
-info = dicominfo('40651632');
-I = dicomread(info);
-rgbImage = I(:,:,1:3);
-implay(I);
