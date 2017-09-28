4a3ed5bb248ba45dc20f1e540f2f6a8d56b2fde8
QNghia <nghianq.it@gmail.com>
2017-09-28 23:04:10 +0700

    update project Read DICOM File, save frame image to disk, call matlab and run func to show image, return show result to c# app

--- ReadDICOMFile/ReadApp/ReadApp/MainForm.cs
+++ ReadDICOMFile/ReadApp/ReadApp/MainForm.cs
@@ -9,14 +9,43 @@ namespace ReadApp
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        public void ReadDICOMFile()
+        {
+           // Read DICOM file
             var file = DicomFile.Open("result.dcm");
+            //Get Patient Info
             var name = file.Dataset.Get<string>(DicomTag.PatientAddress);
+            //get list image of dicom file
             var image = new DicomImage("goodSample");
+            //Check Path is exist or not
+            var dataPath = Application.StartupPath + "\\data\\";
+            if (!System.IO.Directory.Exists(dataPath))
+            {
+                System.IO.Directory.CreateDirectory(dataPath);
+            }
+            //Loop to save all image to data folder
             for (int i = 0; i < image.NumberOfFrames; i++)
             {
-                image.RenderImage(i).AsBitmap().Save("/result/" + i + "result.jpg");
+                image.RenderImage(i).AsBitmap().Save(dataPath + i + ".jpg");
             }
-            System.Console.WriteLine(name);
+        }
+
+        public void CallMatlabFunc()
+        {
+            // Create the MATLAB instance 
+            MLApp.MLApp matlab = new MLApp.MLApp();
+
+            matlab.Execute("cd " + Application.StartupPath);
+
+            object isReadOK = null;
+            var imagePath = Application.StartupPath + "\\data\\1.jpg";
+            matlab.Feval("processImage", 1, out isReadOK, imagePath);
+
+            object[] res = isReadOK as object[];
+            bool result = (bool)res[0];
+            System.Console.WriteLine("Result: " + result);
         }
     }
 }
