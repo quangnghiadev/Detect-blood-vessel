bc712a733f0f7c9bb497e4a2fa9e42b2f1282329
QNghia <nghianq.it@gmail.com>
2017-11-28 21:24:36 +0700

    remove unnecessary file

--- acode_fileExchange_941/origin-code.m
+++ acode_fileExchange_941/origin-code.m
@@ -1,39 +1,36 @@
+
+%%
 clear
 clc
-im=imread('86698558_frame15.tiff');
-bw_mask=imread('mask.tif');
+im=imread('vessel.tif');
+bw_mask=imread('vessel_mask.gif');
 bw_mask=logical(bw_mask);
-ref_im=imread('86698558_frame15_output.jpg');
+% ref_im=imread('Retina_drive_1_Ref.gif');
 %
 im=im(:,:,2);
 im=mat2gray(im).*mat2gray(bw_mask);
 im=imcomplement(im);% Assume vessels are lighter than background
 im=im2double(im);
-ref_bw=im2bw(ref_im,0.5);
+% ref_bw=im2bw(ref_im,0.5);
 %%
 DEG_NUM=12;
-LEN_c=10;
-LEN_o=20;
+LEN_c=15; % nghia_dog: cái này chi?u dài c?a linear struct element - ch?nh s?a cái này v?i cái d??i
+LEN_o=15;
 LEN_diff=7;
 %
 ic1=reconstruction_by_dilation(im,LEN_c,DEG_NUM);
-figure,imshow(ic1);title('reconstruction_by_dilation');
-io1=min_openings(im,LEN_o,20);
-
-io1=min_openings(io1,LEN_o,20);
-figure,imshow(io1);title('min openning 20');
-% 
+io1=min_openings(im,LEN_o,DEG_NUM);
 iv=mat2gray(ic1-io1);
-imDiff=smooth_cross_section(iv,17,LEN_c);
+imDiff=smooth_cross_section(iv,LEN_diff,LEN_c);
 imL=reconstruction_by_dilation(imDiff,LEN_c,DEG_NUM);
 imF=reconstruction_by_erosion(imL,LEN_c,DEG_NUM);
 % 
 figure,imshow(iv);title('iv');
 figure,imshow(imDiff);title('imDiff');
-% figure,imshow(imL);title('imL');
-% figure,imshow(imF);title('imF');
-% % %% Hysteresis thresholding
-TH_LOW=12;
+figure,imshow(imL);title('imL');
+figure,imshow(imF);title('imF');
+%% Hysteresis thresholding
+TH_LOW=10;% nghia_dog cái này là ng??ng thích nghi nh? nh?t, gi?m xu?ng thì l?y dc nhi?u h?n
 TH_HIGH=40;
 min_obj=180;
 min_hole=10;
@@ -47,7 +44,6 @@ bw_result=bw_result& bw_mask;
 bw_result = clear_bw(bw_result, min_obj, min_hole);
 %
 figure,imshow(bw_result);title('result');
-
 % r=eval_metrics(bw_result,ref_bw,bw_mask);
-% fprintf('TPR=%g\n FPR=%g\n accuracy=%g\n precision=%g\n',...
-%     r(1),r(2),r(3),r(4));
+fprintf('TPR=%g\n FPR=%g\n accuracy=%g\n precision=%g\n',...
+    r(1),r(2),r(3),r(4));
