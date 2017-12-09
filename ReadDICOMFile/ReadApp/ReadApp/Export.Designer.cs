347d0b1900f0f37e90f5846f6eaedf55e083f94b
luco <tientin995@gmail.com>
2017-12-09 18:42:35 +0700

    done export

---
+++ ReadDICOMFile/ReadApp/ReadApp/Export.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ReadApp
+{
+    partial class Export
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbOptions = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.cbFrame = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cbOptions
+            // 
+            this.cbOptions.FormattingEnabled = true;
+            this.cbOptions.Items.AddRange(new object[] {
+            "1 frame",
+            "all frame"});
+            this.cbOptions.Location = new System.Drawing.Point(112, 25);
+            this.cbOptions.Name = "cbOptions";
+            this.cbOptions.Size = new System.Drawing.Size(121, 21);
+            this.cbOptions.TabIndex = 0;
+            this.cbOptions.SelectedIndexChanged += new System.EventHandler(this.cbOptions_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Options";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Frame";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(112, 134);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 3;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // cbFrame
+            // 
+            this.cbFrame.FormattingEnabled = true;
+            this.cbFrame.Location = new System.Drawing.Point(112, 74);
+            this.cbFrame.Name = "cbFrame";
+            this.cbFrame.Size = new System.Drawing.Size(121, 21);
+            this.cbFrame.TabIndex = 4;
+            // 
+            // Export
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 203);
+            this.Controls.Add(this.cbFrame);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbOptions);
+            this.Name = "Export";
+            this.Text = "Export";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbOptions;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.ComboBox cbFrame;
+    }
+}
---
+++ ReadDICOMFile/ReadApp/ReadApp/Export.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ReadApp.Manager;
+using System.Drawing.Imaging;
+
+namespace ReadApp
+{
+    public partial class Export : Form
+    {
+        String filePath;
+        public Export()
+        {
+            InitializeComponent();
+            cbOptions.SelectedIndex = 1;
+            int[] arr = Enumerable.Range(1, DICOMManager.shared.FrameCount() + 1).ToArray();
+            cbFrame.DataSource = arr;
+        }
+
+        public Export(String path)
+        {
+            InitializeComponent();
+            cbOptions.SelectedIndex = 1;
+            filePath = path;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbOptions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbOptions.SelectedIndex == 0)
+            {
+                cbFrame.Enabled = true;
+            } else
+            {
+                cbFrame.Enabled = false;
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "png(*.png)| *.png; |Bitmap(*.bmp)| *.bmp; | JPG(*.jpg)| *.jpg; | GIF(*.gif)| *.gif; | TIFF(*.tif)| *.tif";
+
+            if (cbOptions.SelectedIndex == 0)
+            {
+                
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    var fullPath = sfd.FileName;
+                    DICOMManager.shared.ExportFrame(cbFrame.SelectedIndex + 1, getFormat(fullPath), fullPath);
+                }
+            } else if(cbOptions.SelectedIndex == 1)
+            {
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    var fullPath = sfd.FileName;
+                    var fileType = fullPath.Split('.').Last();
+                    var folderPath = fullPath.Replace("."+fileType , "\\");
+                    if(fileType.CompareTo("gif") ==0)
+                    {
+                        DICOMManager.shared.ExportGifFile(fullPath);
+                    }
+                    else
+                    {
+                        DICOMManager.shared.ExportAllFrame(getFormat(fullPath), folderPath);
+                    }
+                    
+                }
+            }
+
+
+        }
+        
+        private ImageFormat getFormat(String filePath)
+        {
+            var stringType = filePath.Split('\\').Last();
+            if (stringType.CompareTo("png") == 0)
+            {
+                return ImageFormat.Png;
+            }
+
+            if (stringType.CompareTo("bmp") == 0)
+            {
+                return ImageFormat.Bmp;
+            }
+
+            if (stringType.CompareTo("jpg") == 0)
+            {
+                return ImageFormat.Jpeg;
+            }
+
+            if (stringType.CompareTo("gif") == 0)
+            {
+                return ImageFormat.Gif;
+            }
+
+            if (stringType.CompareTo("tif") == 0)
+            {
+                return ImageFormat.Tiff;
+            }
+
+            return ImageFormat.Jpeg;
+        }
+
+
+    }
+}
