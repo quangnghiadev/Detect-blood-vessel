ec09f6422eb811ed19d5705aa603248b0da6f26d
luco <tientin995@gmail.com>
2017-12-16 21:10:32 +0700

    add acurancy

--- ReadDICOMFile/ReadApp/ReadApp/bin/Debug/matlab/calculate_accuracy.m
+++ ReadDICOMFile/ReadApp/ReadApp/bin/Debug/matlab/calculate_accuracy.m
@@ -1,5 +1,17 @@
 function ratio = calculate_accuracy()
 %% Add necessary paths
+
 addpath(genpath('./data'));
-ratio = 96.69;
+img = im2bw(imread('result.tif'));
+truth_img = im2bw(imread('groundtruth.jpg'));
+truth_img=im2bw(truth_img,0.5);
+ P = find(truth_img == 1 & img == 1);
+ P1 = find(truth_img == 1);
+ P2= find(img == 1);
+    
+ common = size(P,1);
+ cm = size(P1,1);
+ co = size(P2,1);
+ ratio = (2*common)/(cm+co);
+
 end
